b7642a21440a8dad62df432d99f63ee6f04b230e
Tusio Andriamiadana <tusandri@c1r7s7.42antananarivo.mg>
2026-05-02 11:00:02 +0300

    dragon 3

--- python_module/Py02/ex2/ft_different_errors.py
+++ python_module/Py02/ex2/ft_different_errors.py
@@ -1,6 +1,29 @@
+def operation_set(operation):
+    if (operation == 0):
+        value = "abc"
+        value = int(value)
+    elif (operation == 1):
+        value = 10
+        value = value/0
+    elif (operation == 2):
+        with open('fichier_inexistant.txt', 'r') as file:
+            value = file.read()
+    elif(operation == 3):
+        Value = "Sdsfoije" + 12
+    else:
+        print("successfuly operation number set ;)")
 
 
-def test_error_types(num_error:int):
-    if(num_error == 0):
+def test_error_types(Value:int)->int:
+    try:
+        operation_set(Value)
+        return (Value)
 
-def garden_operations(operation_number:int):
+    except (ValueError, ZeroDivisionError, FileNotFoundError, TypeError) as e:
+        print(f"Caught TypeError: {e}")
+        return (0)
+
+def garden_operations(operation_number:int)->int:
+    if (test_error_types(operation_number)):
+        return (operation_number)
+    return (-1) #irreur signification !
