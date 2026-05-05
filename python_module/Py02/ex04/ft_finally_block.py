726abbc750eb9502d6e43fe731e9c9563ac92531
Tusio Andriamiadana <tusandri@c1r7s2.42antananarivo.mg>
2026-05-06 00:16:58 +0300

    sdf

--- python_module/Py02/ex04/ft_finally_block.py
+++ python_module/Py02/ex04/ft_finally_block.py
@@ -1,2 +1,31 @@
-def water_plant(plant_name):
-    
+class GardenError(Exception):
+	def __init__(self, message = "Erreur, Plante inconnue :("):
+		super().__init__(message)
+
+
+class PlantError(GardenError):
+	pass
+
+
+class WaterError(GardenError):
+	pass
+
+
+def water_plant(plant_name: str) -> str:
+    if (plant_name != plant_name.capitalize()):
+        print("[OK]")
+        plant_name = plant_name.capitalize()
+        return (plant_name)
+    else:
+        raise WaterError("Erreur, plante non arroser :(")
+    return (plant_name)
+def test_watering_system():
+    print("== Opens the watering system ==")
+    test_list = ("tomate", "lettuce", "carrots", "tomato", "1ettuce")
+    for test in test_list:
+        try:
+            water_plant(test)
+        except WaterError as e:
+            print(f"{e}")
+
+test_watering_system()
