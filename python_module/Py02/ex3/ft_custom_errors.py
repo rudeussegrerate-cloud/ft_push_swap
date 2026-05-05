ac261949eaa6280e78fe0284aff7e64790a0bcec
Tusio Andriamiadana <tusandri@c1r10s6.42antananarivo.mg>
2026-05-05 16:41:44 +0300

    gg

--- python_module/Py02/ex3/ft_custom_errors.py
+++ python_module/Py02/ex3/ft_custom_errors.py
@@ -1,30 +1,46 @@
 class GardenError(Exception):
-    def __init__(self, message = "Error Garden"):
-        self.message = message
-        super().__init__(self.message)
+	def __init__(self, message = "Erreur, Plante inconnue :("):
+		super().__init__(message)
 
 
 class PlantError(GardenError):
-    def __init__(self, message = "Plante Error")
-        super().__init__(message)
+	pass
 
 
 class WaterError(GardenError):
-    def __init__(self, message = "Water Error"):
-        super().__init__(message)
+	pass
+
+
+def raiseError(Errorexcept: GardenError, message)->None:
+	if (message):
+		raise Errorexcept(message)
+	else:
+		raise Errorexcept()
+
 
 class Plant:
 	def __init__(self, name:str, height:float, p_age:int)->None:
-		if (height < 0 or self.is_aYear(p_age)):
-			print("Erreur init OBJ")
+		if (not name or not height or not p_age):
+			raiseError(GardenError, '')
+		if (height < 0 or not self.is_aYear(p_age)):
+			raiseError(PlantError, "Entrer une valeur correrct de la taille ou de l'age")
 			self._name = "None"
 			self._height = 0.0
 			self._p_age = 0
 		else:
 			self._name = str(name)
-			self._height = float(height)
+			self._height = round(float(height), 1)
 			self._p_age = p_age
 		self._stat = Plant.stat()
+		self._waterqty = 0
+
+	def get_waterqty(self):
+		return self._waterqty
+
+	def set_waterqty(self, value):
+		if (value < 25):
+			raiseError(WaterError, "La plante manque d'eau et risque de fanner")
+		self._waterqty = value
 
 	class stat:
 		def __init__(self):
@@ -73,17 +89,15 @@ class Plant:
 
 	def	set_height(self, height:float)->None:
 		if (height < 0):
-			print("Error, height value is wrong")
+			raiseError(PlantError, "Error, height value is wrong")
 		else:
 			self._height = height
-			print(f"Height update: {self._height}")
 
 	def	set_age(self, age:int)->None:
 		if (self.is_aYear(age)):
-			print("Error, age value is wrong")
+			raiseError(PlantError, "Error, age value is wrong")
 		else:
 			self._p_age = age
-			print(f"Age update: {self._p_age}")
 
 	def	get_height(self)->float:
 		return (self._height)
@@ -93,3 +107,29 @@ class Plant:
 
 	def stat_plant(self):
 		print(f"show: {self._stat.get_show()}, grow: {self._stat.get_grow()}, age: {self._stat.get_age()}")
+
+def test_operation(operation_number: int) -> None:
+	if (operation_number == 0):
+		plante = Plant("asf", 364, -2)
+	elif (operation_number == 1):
+		plante = Plant('Rose', 10, 366)
+		plante.set_waterqty(operation_number)
+	
+
+if __name__ == "__main__":
+	print("=== Custom Garden Errors Demo ===\n")
+	i = 0
+	a = ''
+	except_list = (PlantError, WaterError, GardenError, GardenError)
+	while i < 4:
+		try:
+			if (i < 3):
+				print(f"Testing {except_list[i].__name__}...")
+				a = '\n'
+			if i > 1:
+				a = ''
+			test_operation(i%2)
+		except except_list[i] as e:
+			print(f"Caught {except_list[i].__name__}: {e}!{a}")
+		i += 1
+	print("\nAll custom error types work correctly!")
