3b700cc3bb5ddfb371712450186124d45c055fd6
Tusio Andriamiadana <tusandri@c1r11s6.42antananarivo.mg>
2026-06-29 17:01:11 +0300

    dragon 1

---
+++ python_module/Python/Py03/ex3/ft_plant_factory.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+class Plant:
+    def __init__(self, name: str, height: float, p_age: int):
+        self.name = name
+        self.height = height
+        self.p_age = p_age
+        self.up_grow = 0.4
+
+    def grow(self) -> None:
+        if self.name == "Rose":
+            self.up_grow = 0.8
+        elif self.name == "Sunflower":
+            self.up_grow = 0.5
+        elif self.name == "Cactus":
+            self.up_grow = 0.2
+        self.height = round(self.height + self.up_grow, 1)
+
+    def age(self) -> None:
+        self.p_age = self.p_age + 1
+
+    def show(self) -> None:
+        print(f"{self.name}: {self.height}cm, {self.p_age} days old")
+
+
+def ft_plant_factory() -> None:
+    plante = [Plant("Rose", 25, 30),
+              Plant("Oak", 200, 365),
+              Plant("Cactus", 5, 90),
+              Plant("Sunflower", 20, 65),
+              Plant("Fern", 15, 120)]
+
+    for i in range(5):
+        print("created: ", end="")
+        plante[i].show()
+
+
+if __name__ == "__main__":
+    print("=== Plant Factory Output ===")
+    ft_plant_factory()
--- python_module/Python/Py03/ex5/ft_data_stream.py
+++
@@ -1,6 +0,0 @@
-from typing import *
-import random
-
-# def gen_event(gen: Dict[str, str]):
-
-etudiants = ["Alice", "Bob", "Charlie", "Diana"]
